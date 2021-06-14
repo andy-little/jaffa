15321fc3c21401dd77791f0bdf5a9a4f7177876d
andy-little <69594249+andy-little@users.noreply.github.com>
2021-06-14 16:37:00 +0100

    added static root

--- jaffa/settings.py
+++ jaffa/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 
 """ AWS_ACCESS_KEY_ID = os.environ.get('JAFFA_AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('JAFFA_AWS_SECRET_ACCESS_KEY')
